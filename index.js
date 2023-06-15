e67e11925de3185f54cb3dd687c892af2c4b8d07
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-06-15 02:19:13 -0400

    added Issues solved comments for issues #2, #5, #6, #7, #8, #9, #10, #11, #12, and #13

--- index.js
+++ index.js
@@ -1,77 +1,88 @@
 let table = document.getElementById("table")
 
-function addRow(){
+//Issue #9 Solved
+//add rows to the grid
+function addRow() {
     let columns = table.rows[0].cells.length;
     let row = table.insertRow(); //insertRow is a method alrdy defined in js
 
-    for (let i=0; i<columns; i++){
+    for (let i = 0; i < columns; i++) {
         const cell = row.insertCell() //insertCell makes another table
         cell.innerHTML = ""
     }
 }
-function removeRow(){
+
+//Issue #9 Solved
+//remove rows from the grid
+function removeRow() {
     let rows = table.rows.length
-    if(rows>1){
-        table.deleteRow(rows-1)
+    if (rows > 1) {
+        table.deleteRow(rows - 1)
     }
 }
-function addColumn(){
+
+//Issue #9 Solved
+//add columns to the grid
+function addColumn() {
     let rows = table.rows.length;
 
-    for(let i=0; i < rows; i++){
+    for (let i = 0; i < rows; i++) {
         const cell = table.rows[i].insertCell();
         cell.innerHTML = "";
     }
 }
-function removeColumn(){
+//Issue #7 Solved
+//remove columns from the grid
+function removeColumn() {
     let rowsCount = table.rows.length;
-    
+
     if (rowsCount > 1) {
         let columnsCount = table.rows[0].cells.length;
-        
+
         for (let i = 0; i < rowsCount; i++) {
-        
-        table.rows[i].deleteCell(columnsCount - 1);
+
+            table.rows[i].deleteCell(columnsCount - 1);
         }
     }
 }
-// function clearAll(){
-//     var rowsCount = table.rows.length;
 
-//     for (var i = 0; i < rowsCount; i++) {
-//       table.deleteRow();
-//     }
-// }
-function colorSelection(){
+//Issue #9 Solved
+//select a color from a dropdown menu of colors
+function colorSelection() {
     const Colors = document.getElementById('colors')
     let color = Colors.value.toLowerCase();
     return color;
 }
-table.addEventListener("click", (e) =>{
+
+//Issue #9 Solved
+//click on a single cell, changing its color to the currently selected color
+table.addEventListener("click", (e) => {
     let clickCell = e.target;
-    if(clickCell.tagName === "TD"){
-        clickCell.style.backgroundColor= colorSelection();
+    if (clickCell.tagName === "TD") {
+        clickCell.style.backgroundColor = colorSelection();
     }
 })
 
-//changing the color of cells
-document.getElementById("newButton").addEventListener("click", () =>{
+//Issue #10 Solved
+//changing the color of uncolored/transparent cells
+//fill all uncolored cells with the currently selected color
+document.getElementById("newButton").addEventListener("click", () => {
     let cells = table.getElementsByTagName("TD")
     //cells are the array
-    for(let i=0; i<cells.length; i++){
+    for (let i = 0; i < cells.length; i++) {
         const individualCell = cells[i];
-        if((!individualCell.style.backgroundColor) || individualCell.style.backgroundColor === "transparent" ){ //if it isnt colored in
-            individualCell.style.backgroundColor=colorSelection(); //then color in
+        if ((!individualCell.style.backgroundColor) || individualCell.style.backgroundColor === "transparent") { //if it isnt colored in
+            individualCell.style.backgroundColor = colorSelection(); //then color in
         }
-        
+
     }
 })
 
-//
+//Issue #11 Solved
 //fill all cells with the currently selected color
-document.getElementById("fillAll").addEventListener("click" , () => {
+document.getElementById("fillAll").addEventListener("click", () => {
     let cells = table.getElementsByTagName("TD")
-    for(let i=0; i<cells.length; i++){
+    for (let i = 0; i < cells.length; i++) {
         const individualCell = cells[i];
         individualCell.style.backgroundColor = colorSelection();
     }
@@ -81,7 +92,7 @@ document.getElementById("fillAll").addEventListener("click" , () => {
 //clear all cells/restore all cells to their original/initial color
 document.getElementById("clear").addEventListener("click", () => {
     let cells = table.getElementsByTagName("TD")
-    for(let i=0; i<cells.length; i++){
+    for (let i = 0; i < cells.length; i++) {
         const individualCell = cells[i];
         individualCell.style.backgroundColor = "transparent"; //then color in
     }
@@ -94,24 +105,32 @@ document.getElementById("clear").addEventListener("click", () => {
 
 let holding = false;
 table.addEventListener("mousedown", (event) => {
-  let cell = event.target;
-  if (cell.tagName === "TD") {
-    holding = true;
-    cell.style.backgroundColor = colorSelection();
-  }
+    let cell = event.target;
+    if (cell.tagName === "TD") {
+        holding = true;
+        cell.style.backgroundColor = colorSelection();
+    }
 });
 
 table.addEventListener("mouseover", (event) => {
-  let cell = event.target;
-  if (holding && cell.tagName === "TD") {
-    cell.style.backgroundColor = colorSelection();
-  }
+    let cell = event.target;
+    if (holding && cell.tagName === "TD") {
+        cell.style.backgroundColor = colorSelection();
+    }
 });
 
 table.addEventListener("mouseup", () => {
-  holding = false;
+    holding = false;
 });
 
 document.addEventListener("mouseup", () => {
-  holding = false;
+    holding = false;
 });
+
+// function clearAll(){
+//     var rowsCount = table.rows.length;
+
+//     for (var i = 0; i < rowsCount; i++) {
+//       table.deleteRow();
+//     }
+// }
